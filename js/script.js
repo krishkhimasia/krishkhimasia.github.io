fb6aeab0dd39983825b2c1de5ad7c5b8d0fdbf70
Krish <khimasiakrish@gmail.com>
2022-05-11 11:17:10 +0530

    Typing effect on welcome

--- js/script.js
+++ js/script.js
@@ -2,3 +2,10 @@ function onClickMenu(){
     document.getElementById("menu").classList.toggle("icon");
     document.getElementById("nav").classList.toggle("change");
 }
+
+var typed = new Typed(".auto-type", {
+    strings: ["Welcome To My Portfolio!"],
+    typeSpeed: 150,
+    backSpeed: 150,
+    loop: false
+})
